556608b01f0352ce0c2a30d85a50c1024ab91165
codeSyre <kodzaimatthewt@gmail.com>
2024-11-10 06:44:57 +0200

    Initial Commit

---
+++ alphagenesisTests/alphagenesisTests.swift
@@ -0,0 +1,17 @@
+//
+//  alphagenesisTests.swift
+//  alphagenesisTests
+//
+//  Created by Yani Development Team on 10/11/2024.
+//
+
+import Testing
+@testable import alphagenesis
+
+struct alphagenesisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
