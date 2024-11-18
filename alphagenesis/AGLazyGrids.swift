c23c1a1a543046f6d53a6769bb0f637c721ec09e
codeSyre <kodzaimatthewt@gmail.com>
2024-11-18 20:12:10 +0200

    I learnt how to use Lazy VGrid & HGrid in SwiftUI.

---
+++ alphagenesis/AGLazyGrids.swift
@@ -0,0 +1,41 @@
+//
+//  AGLazyGrids.swift
+//  alphagenesis
+//
+//  Created by codeSyre on 18/11/2024.
+//
+
+import SwiftUI
+
+struct AGLazyGrids: View {
+    let rows = [GridItem(.fixed(30)), GridItem(.fixed(30))]
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    var body: some View {
+        ScrollView(.horizontal){
+            LazyHGrid(rows: rows){
+                ForEach(0x1f600...0x1f679, id: \.self){value in
+                    Text(String(format: "%x", value))
+                    Text(emoji(value)).font(.largeTitle)
+                }
+            }
+        }
+        
+        ScrollView(.vertical){
+            LazyVGrid(columns: columns){
+                ForEach(0x1f600...0x1f679, id: \.self){value in
+                    Text(String(format: "%x", value))
+                    Text(emoji(value)).font(.largeTitle)
+                }
+            }
+        }
+    }
+    
+    private func emoji(_ value: Int) -> String {
+        guard let scalar = UnicodeScalar(value) else { return "?" }
+        return String(Character(scalar))
+    }
+}
+
+#Preview {
+    AGLazyGrids()
+}
