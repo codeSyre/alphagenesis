bb6ec8c8ca06118377c85b58fb6073b14061f97d
codeSyre <kodzaimatthewt@gmail.com>
2024-11-19 21:17:24 +0200

    Played around with the SafeArea parameters in SwiftUI.

--- alphagenesis/AGLazyGrids.swift
+++ alphagenesis/AGLazyGrids.swift
@@ -11,23 +11,29 @@ struct AGLazyGrids: View {
     let rows = [GridItem(.fixed(30)), GridItem(.fixed(30))]
     let columns = [GridItem(.flexible()), GridItem(.flexible())]
     var body: some View {
-        ScrollView(.horizontal){
-            LazyHGrid(rows: rows){
-                ForEach(0x1f600...0x1f679, id: \.self){value in
-                    Text(String(format: "%x", value))
-                    Text(emoji(value)).font(.largeTitle)
+        ZStack{
+            ScrollView(.horizontal){
+                LazyHGrid(rows: rows){
+                    ForEach(0x1f600...0x1f679, id: \.self){value in
+                        Text(String(format: "%x", value))
+                        Text(emoji(value)).font(.largeTitle)
+                    }
+                }
+            }
+            
+            ScrollView(.vertical){
+                LazyVGrid(columns: columns){
+                    ForEach(0x1f600...0x1f679, id: \.self){value in
+                        Text(String(format: "%x", value))
+                        Text(emoji(value)).font(.largeTitle)
+                    }
                 }
             }
         }
-        
-        ScrollView(.vertical){
-            LazyVGrid(columns: columns){
-                ForEach(0x1f600...0x1f679, id: \.self){value in
-                    Text(String(format: "%x", value))
-                    Text(emoji(value)).font(.largeTitle)
-                }
-            }
-        }
+        .background(
+            .blue
+        )
+        .ignoresSafeArea(edges: .top)
     }
     
     private func emoji(_ value: Int) -> String {
