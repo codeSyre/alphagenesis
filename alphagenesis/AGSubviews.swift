afdf15720eed23e7d97ef96b81c9987eb099c5c2
codeSyre <kodzaimatthewt@gmail.com>
2024-11-20 21:46:35 +0200

    I learnt how to make and manipulate subviews, extract functions & use @Binding with @State variables in SwiftUI.

---
+++ alphagenesis/AGSubviews.swift
@@ -0,0 +1,60 @@
+//
+//  AGSubviews.swift
+//  alphagenesis
+//
+//  Created by codeSyre on 20/11/2024.
+//
+
+import SwiftUI
+
+struct AGSubviews: View {
+    @State var text: String = "codeSyre"
+    @State var btnTitle: String = "Full Name"
+    @State var isClicked: Bool = false
+    var body: some View {
+        VStack{
+            Text(text)
+                .font(.largeTitle)
+                .padding()
+            ButtonText(text: $text, btnTitle: $btnTitle, isClicked: $isClicked)
+        }
+    }
+    
+    
+}
+
+struct ButtonText: View {
+    @Binding var text: String
+    @Binding var btnTitle: String
+    @Binding var isClicked: Bool
+    var body: some View {
+        Button(
+            action: {
+                changeText()
+            }, label: {
+                Text(btnTitle)
+                    .foregroundColor(.white)
+            }
+        )
+        .padding()
+        .padding(.horizontal, 20)
+        .background(.blue)
+        .cornerRadius(10)
+    }
+    
+    func changeText() -> Void{
+        if isClicked {
+            text = "Syre Wolf"
+            btnTitle = "Username"
+            isClicked = !isClicked
+        } else {
+            text = "codeSyre"
+            btnTitle = "Full Name"
+            isClicked = !isClicked
+        }
+    }
+}
+
+#Preview {
+    AGSubviews()
+}
